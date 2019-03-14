bda082bc401a1c4c3f8fe4b596bd8dcd3343be92
Mateusz <mateusz.kupper@gmail.com>
2019-03-14 22:31:51 +0000

    1dconv

--- parameters/architectures.py
+++ parameters/architectures.py
@@ -185,3 +185,23 @@ class Nature_Paper_Conv_Dropout:
 
     def __str__(self):
         return "Architecture used in the nature paper in 2015 with dropout on 2nd conv layer, 0.7 keep prob."
+
+
+class One_D_Conv:
+
+    def evaluate(self, input, action_size):
+        layer1_out = tf.layers.conv1d(input, filters=32, kernel_size=[8],
+            strides=[4], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer1_out')
+        layer2_out = tf.nn.dropout(tf.layers.conv1d(layer1_out, filters=64, kernel_size=[4],
+            strides=[2], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer2_out'), .5, name='layer2_out')
+        layer3_out = tf.layers.conv1d(layer2_out, filters=64, kernel_size=[3],
+            strides=[1], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer3_out')
+        layer4_out = tf.layers.dense(tf.layers.flatten(layer3_out), 512, activation=tf.nn.relu)
+        output =  tf.layers.dense(layer4_out, action_size, activation=None)
+        return output
+
+    def __str__(self):
+        return "Architecture used in the nature paper in 2015 with dropout on 2nd conv layer, 0.7 keep prob."
